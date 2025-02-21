da1af1f574b4a5e048c43a9f5852e6ac73b53848
plfgit <2043618@student.uma.pt>
2025-02-21 10:16:11 +0000

    changed message again

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, GitHub test!");
+            Console.WriteLine("2nd change");
         }
     }
 }
